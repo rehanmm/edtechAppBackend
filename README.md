944c9bf9b3416e5a2c95fa618c25dcfddf138dc9
rehanmm <21bcs049@nith.ac.in>
2022-08-03 17:13:36 +0530

    config error

---
+++ README.md
@@ -0,0 +1,2 @@
+APIs Docs
+https://docs.google.com/document/d/1guyII786KSvr5n_6gjAGA5av8eXVMG3pLCxWvTX2gd0/edit?usp=sharing
---
+++ backend/config/config.js
@@ -0,0 +1,5 @@
+module.exports={
+    MONGO_URL:"mongodb://localhost:27017/Edtech",
+PORT:3000,
+JWT_SECRET_KEY:"jhfeiufevruybebvkvjelvkevjdkenl"
+}
