a192354616c15cc9c74a6066501c32efdf749852
rehanmm <21bcs049@nith.ac.in>
2022-08-03 18:00:51 +0530

    damn

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,4 +1,4 @@
 
 exports.PORT = process.env.PORT || 3000;
-exports.MONGODB_URI = process.env.MONGODB_URI||'mongodb://localhost:27017/Edtech';
+exports.MONGODB_URI = process.env.MONGODB_URI;
 exports.NODE_ENV = process.env.NODE_ENV;
