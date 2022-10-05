a758e035d7ad595a639c4902a3d82af6593987c1
aditya-nith <97960056+aditya-nith@users.noreply.github.com>
2022-10-05 05:57:01 -0700

    is personality test

--- backend/controllers/personalityCtrl.js
+++ backend/controllers/personalityCtrl.js
@@ -14,7 +14,7 @@ getPersonality =  catchAsyncError(async (req, res, next) => {
     if (!user) {
         return next(new errorHandler('list not found', 200))
     }
-    if (user.is_personality_test_taken && (user.personality != null && !user.personality)){
+    if (user.is_personality_test_taken){
         return res.status(200).json(
             {
                 success: true,
