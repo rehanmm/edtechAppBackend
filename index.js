5be881eb521c04590250ac4e1a795b91c1aab489
rehanmm <21bcs049@nith.ac.in>
2022-12-13 01:33:47 +0530

    vercel.json update

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const app = require('./backend/server');
 
-app.listen(config.PORT, function (err) {
+app.listen(3000, function (err) {
     if(err){
         console.log("error while starting server");
         console.log(err);
         console.log(err.stack);
     }
     else{
-        console.log("server has been started at port "+config.PORT);
+        console.log("server has been started at port "+3000);
     }
 })
